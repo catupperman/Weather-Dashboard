acd12d09f8a1ef3f82bfea42e7fa2eae576db130
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-07-09 14:29:50 -0400

    adding shell HTML and creating javascript todo

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+//TODO: a weather dashboard with form inputs
+//TODO: search for a city, presented with current and future conditions for that city and that city is added to the search history
+//TODO:view current weather conditions for that city, presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//TODO view the UV index, presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//TODO: view future weather conditions for that city, presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//TODO: click on a city in the search history, again presented with current and future conditions for that city
